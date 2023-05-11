1eb6f145d5ad2e88283bcbbc7c264a427b73e1cd
Georgios E. Syros <geo2001s@gmail.com>
2023-05-11 19:04:51 +0300

    Implemented visual feedback for clickable elements

--- src/frontend/js/index.js
+++ src/frontend/js/index.js
@@ -7,6 +7,83 @@ const presetsbutton = document.getElementById('presetsbutton');
 const heatdefrostinterface = document.getElementById('heatinterface');
 const presetsinterface = document.getElementById('presetsinterface');
 
+const amount = 20; // amount to darken the background color of the element 
+
+function darkenColor(hexColor, amount) {
+    // Remove the "#" character if present
+    hexColor = hexColor.replace("#", "");
+  
+    // Convert the hex color to RGB
+    const r = parseInt(hexColor.substring(0, 2), 16);
+    console.log(hexColor.substring(0, 2))
+    const g = parseInt(hexColor.substring(2, 4), 16);
+    console.log(hexColor.substring(2, 4))
+    const b = parseInt(hexColor.substring(4, 6), 16);
+    console.log(hexColor.substring(4, 6))
+  
+    // Calculate the darkened RGB values
+    const darkenedR = Math.max(r - amount, 0);
+    const darkenedG = Math.max(g - amount, 0);
+    const darkenedB = Math.max(b - amount, 0);
+  
+    // Convert the darkened RGB values back to hex
+    const darkenedHexColor = `#${darkenedR.toString(16)}${darkenedG.toString(16)}${darkenedB.toString(16)}`;
+  
+    return darkenedHexColor;
+}
+
+function lightenColor(hexColor, amount) {
+    // Remove the "#" character if present
+    hexColor = hexColor.replace("#", "");
+  
+    // Convert the hex color to RGB
+    const r = parseInt(hexColor.substring(0, 2), 16);
+    const g = parseInt(hexColor.substring(2, 4), 16);
+    const b = parseInt(hexColor.substring(4, 6), 16);
+  
+    // Calculate the lightened RGB values
+    const darkenedR = Math.max(r + amount, 0);
+    const darkenedG = Math.max(g + amount, 0);
+    const darkenedB = Math.max(b + amount, 0);
+  
+    // Convert the darkened RGB values back to hex
+    const darkenedHexColor = `#${darkenedR.toString(16)}${darkenedG.toString(16)}${darkenedB.toString(16)}`;
+  
+    return darkenedHexColor;
+}
+
+function rgbToHex(rgb) {
+
+    rgb = rgb.replace("rgb(", "").replace(")", "").split(", ")
+
+    const toHex = (value) => {
+      const hex = value.toString(16);
+      return hex.length === 1 ? "0" + hex : hex;
+    };
+  
+    const red = toHex(parseInt(rgb[0]));
+    const green = toHex(parseInt(rgb[1]));
+    const blue = toHex(parseInt(rgb[2]));
+  
+    return `#${red}${green}${blue}`;
+}
+
+// Simulate on touchstart and touchend events on all 'clickable' elements:
+
+const clickables = document.querySelectorAll('.clickable');
+
+// For all clickable elements reduce and increase the background color on touchstart and touchend respectively:
+
+clickables.forEach((clickable) => {
+    clickable.addEventListener('touchstart', () => {
+        clickable.style.backgroundColor = darkenColor(rgbToHex(window.getComputedStyle(clickable).backgroundColor), amount)
+    });
+    
+    clickable.addEventListener('touchend', () => {
+        clickable.style.backgroundColor = lightenColor(rgbToHex(window.getComputedStyle(clickable).backgroundColor), amount)
+    });
+});
+
 heatbutton.addEventListener('click', () => {
     // Make bg color orange:
     heatbutton.style.backgroundColor = "#FF7D05";
