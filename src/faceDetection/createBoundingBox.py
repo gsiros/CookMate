51ff93ec9e418222f826301663af0a29873131c8
FotiosBistas <fotis.bistas@gmail.com>
2023-05-14 15:07:04 +0300

    implement real time face detection

---
+++ src/faceDetection/createBoundingBox.py
@@ -0,0 +1,11 @@
+
+def detect_bounding_box(vid):
+    grayscale_image  = cv2.cvtColor(vid, cv2,COLOR_BGR2GRAY) 
+    faces = face_classifier.detectMultiScale(
+        grayscale_image,scaleFactor=1.1,minNeighbors=5,minSize=(40,40)
+    )
+
+    for (x, y, w, h) in faces: 
+        cv2.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4) 
+
+    return faces 
