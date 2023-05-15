46fe77af59c4fe2bb58cc073a29031e72a57972e
Georgios E. Syros <geo2001s@gmail.com>
2023-05-15 16:33:55 +0300

    Changed web-server port to 443.

--- src/webserver/index.js
+++ src/webserver/index.js
@@ -1,7 +1,9 @@
 // Create simple webserver with express and serve index.html which is located one level up in the frontend folder:
 const https = require('https'); 
 const express = require('express');
-const pem = require('pem'); 
+const pem = require('pem');
+
+const port = 443;
 
 pem.createCertificate({ days: 365, selfSigned: true }, (err, keys) => {
 	  if (err) {
@@ -16,8 +18,8 @@ pem.createCertificate({ days: 365, selfSigned: true }, (err, keys) => {
 	    res.sendFile('index.html');
 	  })
 
-  https.createServer({ key: keys.clientKey, cert: keys.certificate }, app).listen(3000, () => {
-	console.log("Listening on port 3000");
+  https.createServer({ key: keys.clientKey, cert: keys.certificate }, app).listen(port, () => {
+	console.log(`Listening on port ${port}`);
   })
 })
 
