354ecd059cc3d5b6f730faa80ccbdb278fba762f
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2023-05-16 01:25:20 +0300

    word tokenization

--- src/frontend/js/voiceCommands/handleVoiceCommands.js
+++ src/frontend/js/voiceCommands/handleVoiceCommands.js
@@ -1,14 +1,24 @@
 
 
+
 export let voiceCommandFunctions = {
     
     handleVoiceCommand(command){
 
 
+        const words = this.wordTokenizer(command); 
+        console.log(JSON.stringify(words));
     },
 
-    tokenizeCommand(command){
+    wordTokenizer(command){
+        // Remove any punctuation inside the sentence and convert to lowercase
+        let transformed = command.replace(/[.,\/#!$%\^&\*;?:{}=\-_`~()]/g," ").toLowerCase();
 
+        // Split the sentence into an array of words using whitespace as the delimiter
+        const words = transformed.trim().split(/\s+/);
+
+        // Return the array of words
+        return words;
     }
 } 
 
